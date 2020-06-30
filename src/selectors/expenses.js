c9c5491dea401fb7c1e952d31a72f2a731b77d47
Lorena PV <lorenapv93@gmail.com>
2020-06-30 12:22:52 +0100

    Filtering by DatesII

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,9 +1,12 @@
+import moment from 'moment';
+
 //Get visible expenses
 export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
+        const createdAtMoment = moment(expense.createdAt);
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof startDate !== 'number' || expense.createdAt <= endDate;;
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
         return textMatch && startDateMatch && endDateMatch;
     }).sort((a, b) => {
         if (sortBy === 'date') {
